57cdc8f08ad627fdeab2cf9abdc4b302a2ce315d
Obscure. <69711181+kritishd8@users.noreply.github.com>
2026-02-26 14:31:50 +0545

    always use brandcolor favicon on browsers

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/logo/logo-favicon.svg", type: "image/svg+xml" },
       { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
